77c83395c768940114281f1d7e2a4a36450f6f46
julianny-favinha <julianny.favinha@gmail.com>
2018-03-08 19:15:21 -0300

    lab0. resolver problema (ou nao) da imagem com novo intervalo de intensidade

---
+++ lab0/lab0.py
@@ -0,0 +1,37 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import misc
+
+# img file
+img_file = sys.argv[1]
+
+# open image file and stores it in a numpy array
+img = misc.imread(img_file)
+
+# compute histogram of img
+bins = np.arange(0, 255, 1)
+frq, edges = np.histogram(img.flatten(), bins)
+
+# plot and save histogram
+ax = plt.subplot()
+ax.bar(edges[:-1], frq, width=np.diff(edges))
+plt.xlabel("Grayscale")
+plt.ylabel("Frequency")
+plt.savefig(img_file.replace('.png', '') + '_histogram.png')
+
+# show image statistics
+print("(Width, Height): ", img.shape)
+print("Minimum intensity: ", img.min())
+print("Maximum intensity: ", img.max())
+print("Medium intensity: ", img.mean())
+
+# negative image
+img_negative = 255 - img
+misc.imsave(img_file.replace('.png', '') + '_negative.png', img_negative)
+
+# new intensity interval image
+#img_new_intensity = (60/255)*img + 120
+img_new_intensity = img
+img_new_intensity[img_new_intensity < 150] = 200
+misc.imsave(img_file.replace('.png', '') + '_new_intensity.png', img_new_intensity)
