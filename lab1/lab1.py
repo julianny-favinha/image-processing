a0fda5957c62f424ef447aeb92b52ac9bb8720c0
julianny-favinha <julianny.favinha@gmail.com>
2018-03-25 15:39:01 -0300

    os pixels que estao abaixo do valor 255 sao parte de algum objeto. modifiquei isso no array

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -8,36 +8,38 @@ file_name = sys.argv[1]
 img_colored = io.imread(file_name)
 
 img_gray = color.rgb2gray(img_colored)
+img_gray = exposure.rescale_intensity(img_gray, out_range=(0.0, 255.0))
+img_gray[img_gray < 255.0] = 0.0
+#plt.imshow(img_gray)
 #plt.imshow(img_gray, cmap="gray")
 #plt.show()
-img_gray = exposure.rescale_intensity(img_gray, out_range=(0, 255))
 
 contours = measure.find_contours(img_gray, 0.8)
-#fig, ax = plt.subplots()
-#ax.imshow(img_gray, interpolation="nearest", cmap=plt.cm.gray)
-#for n, contour in enumerate(contours):
-#	ax.plot(contour[:, 1], contour[:, 0], linewidth=1.5)
-#plt.show()
+fig, ax = plt.subplots()
+ax.imshow(img_gray, interpolation="nearest", cmap=plt.cm.gray)
+for n, contour in enumerate(contours):
+	ax.plot(contour[:, 1], contour[:, 0], linewidth=1.3)
+plt.show()
 
 small_regions = []
 medium_regions = []
 large_regions = []
 areas = []
 
-label_img = measure.label(img_gray, neighbors=4)
+label_img = measure.label(img_gray, neighbors=4, background=255)
 regions = measure.regionprops(label_img)
-print("Number of regions: %d" % (len(regions)-1))
-for properties in regions[1:]:
-	print("Region %d: Perimeter: %.1f Area: %d" % (properties.label - 2, properties.perimeter, properties.area))
+print("Number of regions: %d" % (len(regions)))
+for properties in regions:
+	print("Region %d: Perimeter: %.1f Area: %d" % (properties.label, properties.perimeter, properties.area))
 
 	areas.append(properties.area)
 
 	if properties.area < 1500:
-		small_regions.append(properties.label - 2)
+		small_regions.append(properties.label)
 	elif properties.area < 3000:
-		medium_regions.append(properties.label - 2)
+		medium_regions.append(properties.label)
 	else:
-		large_regions.append(properties.label - 2)
+		large_regions.append(properties.label)
 
 print("Small regions: %d" % (len(small_regions)))
 print(small_regions)
@@ -46,8 +48,8 @@ print(medium_regions)
 print("Large regions: %d" % (len(large_regions)))
 print(large_regions)
 
-plt.hist(areas[1:], bins="auto")
+plt.hist(areas, bins="auto")
 plt.title("Histogram of areas in %s" % (file_name))
-plt.xlabel("Areas")
+plt.xlabel("Area")
 plt.ylabel("Number os regions")
-plt.savefig('histogram_' + file_name)
+plt.savefig("histogram_" + file_name)
