150729a0395f6140db5e619f369b4ab9d121d2c1
julianny-favinha <julianny.favinha@gmail.com>
2018-04-11 22:16:46 -0300

    decode funcionando

--- lab2/code.py
+++ lab2/code.py
@@ -7,21 +7,22 @@ python3 code.py input.png bit_plane text.txt
 INPUT
 - img_input.png: imagem onde sera embutida a mensagem.
 - bit_plane: três planos de bits menos significativos, sendo 0, 1 ou 2
-- text.txt: mensagem a ser oculta.
+- text_input.txt: mensagem a ser oculta.
 
 OUTPUT
 - img_output.png: imagem com mensagem embutida.
-- plane_bits: tres planos de bits menos significativos representados pelos valores 0, 1 ou 2.
+*********************************************************************************************************************
 """
 
+BITS = 8
+
 def toBinary(d):
-    return ''.join(str(1 & int(d) >> i) for i in range(8)[::-1])
+    return ''.join(str(1 & int(d) >> i) for i in range(BITS)[::-1])
 
 def toDecimal(b):
 	return int(b, 2)
 
 def substituteCharAt(string, char, position):
-	#print("string %s, char %c, position %d" %(string, char, position))
 	return string[:position] + char + string[(position+1):]
 
 def save_plane(img_colored, file_name, bit_plane):
@@ -30,19 +31,13 @@ def save_plane(img_colored, file_name, bit_plane):
 		for col in range(0, img_colored.shape[1]):
 			f.write("%d " %(img_colored[row][col][bit_plane]))
 		f.write("\n")
+	f.close()
 
 def readText(file_name):
 	f = open(file_name, "r")
-	return f.read().strip()
+	return f.read()[:-1]
 
-def cor(color):
-	if color == 0:
-		return "red"
-	elif color == 1:
-		return "green"
-	return "blue"
-
-# params
+# parameters
 file_name = sys.argv[1]
 bit_plane = int(sys.argv[2])
 file_text_name = sys.argv[3]
@@ -55,6 +50,7 @@ save_plane(img_colored, "plane_img_blue", 2)
 
 # \0 é a condição de parada para podermos decodificar posteriormente
 message = readText(file_text_name) + "\0"
+
 row = 0
 col = 0
 color = 0
@@ -64,24 +60,25 @@ for letter in message:
 	binary_message += toBinary(ord(letter))
 
 for bit in binary_message:
-	""" pegar o valor  img_colored[x][y] -> array [R, G, B]
-		transformar R para binario, trocar o bit 0 1 ou 2 menos significativo por c
-		voltar R para decimal e colocar na imagem de saida"""
 	# chegou ao final da linha
 	if col == img_colored.shape[1]:
 		row += 1
 		col = 0
 
-	pixelRgb = img_colored[row][col]
-	print(cor(color))
-	print(bit)
-	print(toDecimal(substituteCharAt(toBinary(pixelRgb[color]), bit, 7 - bit_plane)))
-	img_colored[row][col][color] = toDecimal(substituteCharAt(toBinary(pixelRgb[color]), bit, 7 - bit_plane))
+	""" valor R = img_colored[x][y] retorna um array [R, G, B]
+		transformar R para binario, trocar o bit 0, 1 ou 2 menos significativo por c
+		voltar valor novo para decimal e colocar na imagem de saida"""
+	pixel_rgb = img_colored[row][col]
+	img_colored[row][col][color] = toDecimal(substituteCharAt(toBinary(pixel_rgb[color]), bit, BITS - 1 - bit_plane))
+	
+	# passou por todas as cores de uma coluna
 	if color + 1 == 3:
-		col += 1	
+		col += 1
+
 	color = (color + 1) % 3
 
 save_plane(img_colored, "plane_new_red", 0)
 save_plane(img_colored, "plane_new_green", 1)
 save_plane(img_colored, "plane_new_blue", 2)
+io.imsave(file_name.replace(".png", "") + "_output.png", img_colored)
 
--- lab2/decode.py
+++ lab2/decode.py
@@ -2,16 +2,54 @@ import sys
 from skimage import io
 
 """
-python3 decode.py output.png
+python3 decode.py output.png plane_bits
 
 INPUT
 - img_output.png: imagem com a mensagem embutida.
+- plane_bits: tres planos de bits menos significativos representados pelos valores 0, 1 ou 2.
 
 OUTPUT
-- text.txt: mensagem que estava embutida na imagem.
-- plane_bits: tres planos de bits menos significativos representados pelos valores 0, 1 ou 2.
+- text_output.txt: mensagem que estava embutida na imagem.
 """
 
-# reads file image
+BITS = 8
+
+def toDecimal(b):
+	return int(b, 2)
+
+def toBinary(d):
+    return ''.join(str(1 & int(d) >> i) for i in range(BITS)[::-1])
+
+def writeInFile(char):
+	f = open("text_output.txt", "a")
+	f.write(char)
+	f.close()
+
+def getBit(string, bit):
+	return string[BITS - 1 - bit]
+
+# parameters
 file_name = sys.argv[1]
+bit_plane = int(sys.argv[2])
+
+# reads image
 img_colored = io.imread(file_name)
+
+bits = ""
+color = 0
+stop = False
+for row in range(0, img_colored.shape[0]):
+	for col in range(0, img_colored.shape[1]):
+		print("(%d, %d)" %(row, col))
+		for color in range(0, 3):
+			pixel_rgb = img_colored[row][col]
+			bits = bits + getBit(toBinary(pixel_rgb[color]), bit_plane)
+	
+			if len(bits) == 8:
+				if toDecimal(bits) == 0:
+					stop = True
+					break
+				writeInFile(chr(toDecimal(bits)))				
+				bits = ""
+		if stop:
+			break
