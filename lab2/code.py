64def4d58f592653fefef65d27282e67b9a6c3c9
julianny-favinha <julianny.favinha@gmail.com>
2018-04-16 20:16:41 -0300

    Arrumei comentarios

--- lab2/code.py
+++ lab2/code.py
@@ -4,15 +4,19 @@ from skimage import io
 
 BITS = 8
 
+# retorna o valor inteiro da string b, composta apenas por 0s e 1s
 def toBinary(d):
     return ''.join(str(1 & int(d) >> i) for i in range(BITS)[::-1])
 
+# retorna o valor binário do inteiro d em uma string
 def toDecimal(b):
 	return int(b, 2)
 
+# substitui a posicao position de string pelo caractere char
 def substituteCharAt(string, char, position):
 	return string[:position] + char + string[(position+1):]
 
+# le o texto de um arquivo
 def readText(file_name):
 	f = open(file_name, "r")
 	return f.read()[:-1]
@@ -20,49 +24,52 @@ def readText(file_name):
 
 start_time = time.time()
 
-# parameters
+# parametros de entrada
 file_name = sys.argv[1]
 bit_plane = int(sys.argv[2])
 file_text_name = sys.argv[3]
 
-# reads file image
+# le a imagem de entrada
 img_colored = io.imread(file_name)
 
-# \0 is the stop condition do decode after
+# le a mensagem a ser esteganografada na imagem e coloca o caractere \0 ao final, como critério de parada
 message = readText(file_text_name) + "\0"
 
 row = 0
 col = 0
 color = 0
 
+# transforma cada caractere da imagem em sua representação binária
 binary_message = ""
 for letter in message:
 	binary_message += toBinary(ord(letter))
 
+""" valor R = img_colored[x][y] retorna um array [R, G, B]
+	transformar R para binario, trocar o bit 0, 1 ou 2 menos significativo por c
+	voltar valor novo para decimal e colocar na imagem de saida"""
 for bit in binary_message:
-	# reached end of line
+	# chegou ao final da linha
 	if col == img_colored.shape[1]:
 		row += 1
 		col = 0
 
-	# reached end of image
+	# chegou ao final da imagem
 	if row == img_colored.shape[0]:
 		break
 
-	""" valor R = img_colored[x][y] retorna um array [R, G, B]
-		transformar R para binario, trocar o bit 0, 1 ou 2 menos significativo por c
-		voltar valor novo para decimal e colocar na imagem de saida"""
 	pixel_rgb = img_colored[row][col]
 	img_colored[row][col][color] = toDecimal(substituteCharAt(toBinary(pixel_rgb[color]), bit, BITS - 1 - bit_plane))
 	
-	# pass through all the 3 colors of a column
+	# passou por todas as cores de um pixel
 	if color + 1 == 3:
 		col += 1
 
 	color = (color + 1) % 3
 
+# salva imagem de saida
 io.imsave(file_name.replace(".png", "") + "_output.png", img_colored)
 
+# calcula tempo decorrido
 elapsed_time = time.time() - start_time
 print("Elapsed time: %1f s" %(elapsed_time))
 
--- lab2/decode.py
+++ lab2/decode.py
@@ -6,38 +6,42 @@ import numpy as np
 BITS = 8
 MAX_INTENSITY = 255
 
+# retorna o valor inteiro da string b, composta apenas por 0s e 1s
 def toDecimal(b):
 	return int(b, 2)
 
+# retorna o valor binário do inteiro d em uma string
 def toBinary(d):
     return ''.join(str(1 & int(d) >> i) for i in range(BITS)[::-1])
 
+# retorna a bit-ésima posição de string, de trás para frente 
 def getBit(string, bit):
 	return string[BITS - 1 - bit]
 
+
 start_time = time.time()
 
-# parameters
+# parametros de entrada
 file_name = sys.argv[1]
 bit_plane = int(sys.argv[2])
 
-# reads image
+# le a imagem de entrada e cria as duas imagens dos planos de saida
 img_colored = io.imread(file_name)
 img_bit_plane = np.array(img_colored, copy=True)
 img_bit_plane7 = np.array(img_colored, copy=True)
 
-# open file to write message
+# abre o arquivo para escrever a mensagem que será lida
 f = open(file_name.replace(".png", "") + "_text.txt", "w")
 bits = ""
 color = 0
 write_file = True
 
+"""para cada canal do pixel de posicao [row][col], concatena o bit_plane menos significativo em bits
+	se o tamanho de bits chegou em 8, entao podemos ler um char
+	salva o char no arquivo texto de saida"""
 for row in range(0, img_colored.shape[0]):
 	for col in range(0, img_colored.shape[1]):
 		for color in range(0, 3):
-			"""para cada canal do pixel de posicao [row][col], concatena o bit_plane menos significativo em bits
-				se o tamanho de bits chegou em 8, entao podemos ler um char
-				salva o char no arquivo texto de saida"""
 			pixel_rgb = img_colored[row][col]
 			bit = getBit(toBinary(pixel_rgb[color]), bit_plane)
 			img_bit_plane[row][col][color] = int(bit)
@@ -56,12 +60,15 @@ for row in range(0, img_colored.shape[0]):
 f.write("\n")
 f.close()
 
+# transformação de intensidade
+# isso é feito para podermos visualizar melhor as imagens dos planos. caso contrário, elas ficariam escuras demais
 img_bit_plane[img_bit_plane > 0] = MAX_INTENSITY
 img_bit_plane7[img_bit_plane7 > 0] = MAX_INTENSITY
 
+# salva as imagens
 io.imsave(file_name.replace(".png", "") + "_bit_plane" + str(bit_plane) + ".png", img_bit_plane)
 io.imsave(file_name.replace(".png", "") + "_bit_plane7.png", img_bit_plane7)
 
+# calcula tempo decorrido
 elapsed_time = time.time() - start_time
 print("Elapsed time: %1f s" %(elapsed_time))
-
