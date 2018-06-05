ee66d89b14d0b3b8954e08e52573e449b4a871a8
julianny-favinha <julianny.favinha@gmail.com>
2018-06-05 15:51:52 -0300

    Enviando dicionario de parametros para os outros arqs

--- lab4/geometric_transform.py
+++ lab4/geometric_transform.py
@@ -5,26 +5,41 @@ from bilinear import bilinear
 from bicubic import bicubic
 from lagrange import lagrange
 
+def create_dict(angle, scale, dimensions, mode, inputname, outputname):
+	dic = {}
+
+	dic["angle"] = angle
+	dic["scale"] = scale
+	dic["dimensions"] = dimensions
+	dic["mode"] = mode
+	dic["inputname"] = inputname
+	dic["outputname"] = outputname
+
+	return dic
+
 def read_parameters():
 	parser = argparse.ArgumentParser()
+
 	parser.add_argument("-a", "--angle", type=float, help="angle in degrees to rotate image")
 	parser.add_argument("-e", "--scale", type=float, help="scale to shrink or extend image")
 	parser.add_argument("-d", "--dimensions", nargs="+", type=int, help="width and height of new image")
 	parser.add_argument("-m", "--mode", type=str, help="Interpolation {neighbor, bilinear, bicubic, lagrange}")
 	parser.add_argument("-i", "--inputname", type=str, help="Image input name")
 	parser.add_argument("-o", "--outputname", type=str, help="Image output name")
+
 	args = parser.parse_args()
-	return args.angle, args.scale, args.dimensions, args.mode, args.inputname, args.outputname
+
+	return create_dict(args.angle, args.scale, args.dimensions, args.mode, args.inputname, args.outputname)
 
 if __name__ == "__main__":
 	# parse dos parametros
-	angle, scale, dimensions, mode, input_name, output_name = read_parameters()
+	dic = read_parameters()
 
-	if mode == "neighbor":
-		neighbor()
-	elif mode == "bilinear":
-		bilinear()
-	elif mode == "bicubic":
-		bicubic()
+	if dic["mode"] == "neighbor":
+		neighbor(dic)
+	elif dic["mode"] == "bilinear":
+		bilinear(dic)
+	elif dic["mode"] == "bicubic":
+		bicubic(dic)
 	else:
-		lagrange()
+		lagrange(dic)
