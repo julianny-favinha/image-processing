c28a86a0854bcb0eb7a156ea57147ceb7bc790af
julianny-favinha <julianny.favinha@gmail.com>
2018-06-05 15:51:32 -0300

    Interp vizinho

--- lab4/neighbor.py
+++ lab4/neighbor.py
@@ -1,2 +1,17 @@
-def neighbor():
+import numpy as np
+from skimage import io, exposure
+
+def neighbor(dic):
 	print("neighbor")
+
+	# le a imagem de entrada
+	img = io.imread(dic["inputname"])
+
+	# dimensoes da nova imagem
+	new_img = np.zeros(shape=(int(img.shape[0]*dic["scale"]), int(img.shape[1]*dic["scale"])), dtype=np.uint8)
+
+	for row in range(0, new_img.shape[0]-1):
+		for col in range(0, new_img.shape[1]-1):
+			new_img[row][col] = img[round(row/dic["scale"])][round(col/dic["scale"])]
+
+	io.imsave(dic["outputname"], new_img)
