137f99229274dda0ee809fece260c7571b547959
julianny-favinha <julianny.favinha@gmail.com>
2018-06-06 08:24:16 -0300

    arquivos aux

---
+++ lab4/test.py
@@ -0,0 +1,11 @@
+"""import numpy as np
+
+scale = np.array([[np.cos(30), 0.0],[0.0, 2.0]])
+coord = np.array([[3], [4]])
+x = np.linalg.inv(scale) @ coord
+print(x.ravel()[0])"""
+
+if True:
+	tr = 0
+
+print(tr)
